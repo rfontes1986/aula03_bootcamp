bd618c48361dacd36a1821f809b407874315057a
Rafael Cavalcanti <rfontes86@gmail.com>
2026-06-03 06:18:54 +0100

    updated

--- exercicios.py
+++ exercicios.py
@@ -3,11 +3,26 @@
 # positivos para quantidade e preço. Escreva um programa que verifique esses campos e imprima "Dados válidos" 
 # se ambos forem positivos ou "Dados inválidos" caso contrário.
 
-quantidade = 40
-preco = -20
+#quantidade = 40
+#preco = -20
 
-if quantidade > 0 and preco > 0:
-    print ("valores validos")
-else:
-    print ("valores invalidos")
+#if quantidade > 0 and preco > 0:
+    #print ("valores validos")
+#else:
+    #print ("valores invalidos")
 
+#Exercicio 6: conte quantas vezes cada palavra apareceu em um texto
+
+texto = "hoje o dia amanheceu com um sol lindo mas o tempo pode mudar se em vez do sol aparecer começar a chover, aí não será lindo"
+
+palavras = texto.split(" ")
+
+contagem_palavras = { }
+
+for palavra in palavras:
+    if palavra in contagem_palavras:
+        contagem_palavras[palavra] = +1
+    else:
+        contagem_palavras[palavra] = 1
+
+print(contagem_palavras)
---
+++ exercicios_for.py
@@ -0,0 +1,4 @@
+lista_de_alunos = ["rafael", "fabio", "luciano"]
+
+for aluno in lista_de_alunos:
+    print(aluno)
